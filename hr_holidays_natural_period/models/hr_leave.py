0011ce0b6375a4097de1dcffa8f9cdb622258db8
Alexandre D. Díaz <dev@redneboa.es>
2025-11-03 12:28:17 +0100

    [FIX] hr_holidays_natural_period: not expect a singleton

--- hr_holidays_natural_period/models/hr_leave.py
+++ hr_holidays_natural_period/models/hr_leave.py
@@ -2,6 +2,8 @@
 # Copyright 2024 Tecnativa - Carlos Lopez
 # Copyright 2025 Grupo Isonor - Alexandre D. Díaz
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from collections import defaultdict
+
 from odoo import models
 
 
@@ -12,20 +14,39 @@ class HrLeave(models.Model):
         # We need to set request_unit as 'day'
         # to avoid the calculations being done as hours.
         mod_holidays_status_ids = self.env.context.get("mod_holidays_status_ids", [])
-        old_request_unit = self.holiday_status_id.request_unit
         natural_day_instances = self.filtered(
             lambda x: x.holiday_status_id.id in mod_holidays_status_ids
             or x.holiday_status_id.request_unit
             in ("natural_day", "natural_day_half_day")
         )
-        natural_day_instances.holiday_status_id.sudo().request_unit = (
-            "half_day" if old_request_unit == "natural_day_half_day" else "day"
-        )
+        orig_status_request_units = {
+            leave.holiday_status_id.id: leave.holiday_status_id.request_unit
+            for leave in self
+        }
+
+        for natural_day in natural_day_instances:
+            orig_request_unit = orig_status_request_units[
+                natural_day.holiday_status_id.id
+            ]
+            natural_day.holiday_status_id.sudo().request_unit = (
+                "half_day" if orig_request_unit == "natural_day_half_day" else "day"
+            )
+
         _self = self - natural_day_instances
-        _self = _self.with_context(old_request_unit=old_request_unit)
-        res = super(HrLeave, _self)._get_durations(
-            check_leave_type=check_leave_type, resource_calendar=resource_calendar
-        )
+        leaves_by_orig_ru = defaultdict(lambda: self.env["hr.leave"])
+        for leave in _self:
+            orig_request_unit = orig_status_request_units[leave.holiday_status_id.id]
+            leaves_by_orig_ru[orig_request_unit] += leave
+
+        res = defaultdict(list)
+        for orig_request_unit, leaves in leaves_by_orig_ru.items():
+            _leaves = leaves.with_context(old_request_unit=orig_request_unit)
+            subres = super(HrLeave, _leaves)._get_durations(
+                check_leave_type=check_leave_type, resource_calendar=resource_calendar
+            )
+            for k, v in subres.items():
+                res[k].extend(v)
+
         if not natural_day_instances:
             return res
         _res = super(
@@ -35,5 +56,11 @@ class HrLeave(models.Model):
         )
         for item in natural_day_instances:
             res[item.id] = _res[item.id]
-        natural_day_instances.holiday_status_id.sudo().request_unit = old_request_unit
+
+        for natural_day in natural_day_instances:
+            orig_request_unit = orig_status_request_units[
+                natural_day.holiday_status_id.id
+            ]
+            natural_day.holiday_status_id.sudo().request_unit = orig_request_unit
+
         return res
